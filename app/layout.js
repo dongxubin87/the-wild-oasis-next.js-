94a3e7bfd0960ccd22860e022ed7fc00ff0e978f
dongxubin87 <dongxubin87@gmail.com>
2025-03-02 20:24:44 +0200

    first commit

---
+++ app/layout.js
@@ -0,0 +1,20 @@
+import Logo from "./_components/Logo";
+import Navigation from "./_components/Navigation";
+
+export const metadata = {
+  title: "The Wild Oasis",
+};
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <header>
+          <Logo />
+        </header>
+        <Navigation />
+        <main>{children}</main>
+        <footer>Copyright by the wild oasis</footer>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.js
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>ddd Next the wild oasis!</h1>
+    </div>
+  );
+}

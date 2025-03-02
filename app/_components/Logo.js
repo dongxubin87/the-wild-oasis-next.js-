b8b30e3e04bd03ead2d864c39264832689c6ea9f
dongxubin87 <dongxubin87@gmail.com>
2025-03-02 22:26:05 +0200

    second commit

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -1,10 +1,19 @@
+import Image from "next/image";
 import Link from "next/link";
+import logo from "@/public/logo.png";
 
 function Logo() {
   return (
-    <Link href="/" className="flex items-center gap-4 z-10">
-      <img src="/logo.png" height="60" width="60" alt="The Wild Oasis logo" />
-      <span className="text-xl font-semibold text-primary-100">
+    <Link href="/" className="z-10 flex items-center gap-4">
+      {/* <Image src="/logo.png" height="60" width="60" alt="The Wild Oasis logo" /> */}
+      <Image
+        src={logo}
+        height="60"
+        width="60"
+        quality={100}
+        alt="The Wild Oasis logo"
+      />
+      <span className="text-primary-100 text-xl font-semibold">
         The Wild Oasis
       </span>
     </Link>
--- app/layout.js
+++ app/layout.js
@@ -1,18 +1,34 @@
+import Header from "./_components/Header";
 import Logo from "./_components/Logo";
 import Navigation from "./_components/Navigation";
+import "@/app/_styles/globals.css";
+import { Josefin_Sans } from "next/font/google";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+console.log(josefin);
 
 export const metadata = {
-  title: "The Wild Oasis",
+  title: {
+    template: "%s / The Wild Oasis",
+    default: "Welcome / The Wild Oasis",
+  },
+  description:
+    "Luxurious cabin hotel, located in the heart of the Italian Dolomites, surrounded by beautiful mountains and dark forests",
 };
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        <header>
-          <Logo />
-        </header>
-        <Navigation />
-        <main>{children}</main>
+      <body
+        className={`${josefin.className} bg-primary-950 text-primary-100 flex min-h-screen flex-col antialiased`}
+      >
+        <Header />
+        <div className="flex-1 px-8 py-12">
+          <main className="mx-auto max-w-7xl">{children}</main>
+        </div>
         <footer>Copyright by the wild oasis</footer>
       </body>
     </html>
